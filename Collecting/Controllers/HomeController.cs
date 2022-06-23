c037a47b0c1ef751810ff5d6646aace5962e0c0c
fckscl <dimapugach31102003@gmail.com>
2022-06-23 17:58:51 +0300

    log in

--- Collecting/Controllers/HomeController.cs
+++ Collecting/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Collecting.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Collecting.Controllers
 {
@@ -18,6 +19,7 @@ namespace Collecting.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
