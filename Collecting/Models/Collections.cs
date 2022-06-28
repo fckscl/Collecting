34c02ee239e7399a7fc1b57885fd7155ef6ed950
fckscl <dimapugach31102003@gmail.com>
2022-06-28 21:59:11 +0300

    make admin

---
+++ Collecting/Models/Collections.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Collecting.Models
+{
+    public class Collections
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public IdentityUser User { get; set; }
+        public int UserId { get; set; }
+    }
+}
