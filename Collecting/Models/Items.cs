d4e398d4daea6c2a6e80e38ccbdb94655c606644
fckscl <dimapugach31102003@gmail.com>
2022-07-03 14:06:57 +0300

    add

--- Collecting/Models/Items.cs
+++ Collecting/Models/Items.cs
@@ -3,8 +3,8 @@
     public class Items
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int CollectionId { get; set; }
-        public Collection Collection { get; set; }
+        public Collection? Collection { get; set; }
     }
 }
